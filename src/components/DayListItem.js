ee51d5b1f655370b2a600e47ee9380701a709c37
Shaun Robinson <shaunrobinson@Shauns-MacBook-Air-2.local>
2022-02-21 13:13:42 -0800

    Create function to show number of spots

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -6,16 +6,29 @@ import "components/DayListItem.scss";
 
 export default function DayListItem (props) {
 
+  
   const dayClass = classNames("day-list", {
     "day-list__item": props,
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0
   });
+  
+  const formatSpots = function (spots) {
+    if (spots > 1) {
+      return `${spots} spots remaining`
+    }
+    if (spots === 1) {
+      return `${spots} spot remaining`
+    }
+    if (spots === 0) {
+      return "no spots remaining"
+    }
+  }
 
   return (
     <li className={ dayClass } onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
