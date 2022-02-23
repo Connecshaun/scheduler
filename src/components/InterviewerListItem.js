d23ee2af927a77d78b852b8898d5686c43d87039
Shaun Robinson <shaunrobinson@Shauns-MacBook-Air-2.local>
2022-02-23 11:00:10 -0800

    Styling updates

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,11 +1,20 @@
 import React from "react";
+import "components/InterviewerListItem.scss";
+import classNames from "classnames";
+
 
 export default function InterviewerListItem (props) {
 
+  const interviewerClass = classNames ( "interviewerListItem", {
+    "interviewers__item": props.id,
+    "interviewers__item-image": props.avatar,
+    "interviewers__item--selected": props.selected
+  });
+
   return (
-  <li className="interviewers__item">
-    <img className="interviewers__item-image" src="https://i.imgur.com/LpaY82x.png" alt="Sylvia Palmer"/>
-    Sylvia Palmer
+  <li className={ interviewerClass } onClick={()=> props.setInterviewer(props.id)}>
+    <img className={ interviewerClass } src={ props.avatar } alt={ props.name }/>
+    {props.selected && props.name}
   </li>
   )
 }
