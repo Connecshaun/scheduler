c3069f62732bdbd4893f0b7eaa78f59df40082b4
Shaun Robinson <shaunrobinson@Shauns-MacBook-Air-2.local>
2022-02-22 16:39:26 -0800

    Add userState to <Application>

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import DayList from "./DayList";
 
@@ -24,12 +24,14 @@ export default function Application(props) {
     },
   ];
 
+  const [day, setDay] = useState("Monday")
+
   return (
     <main className="layout">
       <section className="sidebar">
       <img className="sidebar--centered" src="images/logo.png" alt="Interview Scheduler" />
       <hr className="sidebar__separator sidebar--centered" />
-      <nav className="sidebar__menu"><DayList days={days} day= {"Monday"} setDay={day => console.log(day)}/></nav>
+      <nav className="sidebar__menu"><DayList days={ days } day={ day } setDay={ setDay }/></nav>
       <img className="sidebar__lhl sidebar--centered" src="images/lhl.png" alt="Lighthouse Labs" />
       </section>
       <section className="schedule">
