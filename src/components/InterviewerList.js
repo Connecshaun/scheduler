a251e53cebdf82c92c19bb6ab13c6de63913de3d
Shaun Robinson <shaunrobinson@Shauns-MacBook-Air-2.local>
2022-02-23 13:53:21 -0800

    Refactor code

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -6,12 +6,12 @@ import "components/InterviewerList.scss";
 
 export default function InterviewerList (props) {
 
-  const interviewers = props.interviewers.map((interviewer) => <InterviewerListItem key = { interviewer.id } name= { interviewer.name } avatar= { interviewer.avatar } selected = { interviewer.id === props.interviewer } setInterviewer = {event => props.setInterviewer(interviewer.id)} />)
+  const interviewers = props.interviewers.map((interviewer) => <InterviewerListItem key = { interviewer.id } name= { interviewer.name } avatar= { interviewer.avatar } selected = { interviewer.id === props.interviewer } setInterviewer = {() => props.setInterviewer(interviewer.id)} />)
 
   return (
   <section className="interviewers">
     <h4 className="interviewers__header text--light">Interviewer</h4>
-    <ul className="interviewers__list">{interviewers}</ul>
+    <ul className="interviewers__list">{ interviewers }</ul>
   </section>
   )
 }
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -12,10 +12,13 @@ export default function InterviewerListItem (props) {
   });
 
   return (
-  <li className={ interviewerClass } onClick={()=>
-    props.setInterviewer(props.id)}>
-    <img className={ interviewerClass } src={ props.avatar } alt={ props.name }/>
-    {props.selected && props.name}
-  </li>
-  )
+    <li className={ interviewerClass } onClick={ props.setInterviewer }>
+      <img
+        className="interviewers__item-image"
+        src={ props.avatar }
+        alt={ props.name }
+      />
+      { props.selected && props.name }
+    </li>
+  );
 }
