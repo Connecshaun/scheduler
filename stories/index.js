ec79a906728465c5ef7efccbb1b5b8c5e2a37a2a
Shaun Robinson <shaunrobinson@Shauns-MacBook-Air-2.local>
2022-02-24 15:11:30 -0800

    Work on <form /> and aligning story

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 //BUTTON---------------------
 storiesOf("Button", module)
@@ -168,3 +169,9 @@ storiesOf("Button", module)
          .add("Status", () => <Status message="Deleting" />)
 
          .add("Error", () => <Error message="Could not delete appointment" onClose={action("onClose")} />)
+
+         .add("Edit", () =>
+          <Form student="Shaun Robinson" interviewer={4} interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")} />)
+
+        .add("Create", () =>
+          <Form interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")} />)
