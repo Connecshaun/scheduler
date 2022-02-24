da6364601953bf7908a975d0c0399cfb2b422e4d
Shaun Robinson <shaunrobinson@Shauns-MacBook-Air-2.local>
2022-02-23 16:07:38 -0800

    Refactor for controlled lists

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -6,7 +6,9 @@ import "components/InterviewerList.scss";
 
 export default function InterviewerList (props) {
 
-  const interviewers = props.interviewers.map((interviewer) => <InterviewerListItem key = { interviewer.id } name= { interviewer.name } avatar= { interviewer.avatar } selected = { interviewer.id === props.interviewer } setInterviewer = {() => props.setInterviewer(interviewer.id)} />)
+  const {onChange, value} = props
+
+  const interviewers = props.interviewers.map((interviewer) => <InterviewerListItem key = { interviewer.id } name= { interviewer.name } avatar= { interviewer.avatar } selected = { interviewer.id === props.value } setInterviewer = {() => props.onChange(interviewer.id)} />)
 
   return (
   <section className="interviewers">
