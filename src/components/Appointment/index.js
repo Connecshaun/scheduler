bc203ca56ade5da0846ba8b13ca0df93285f4340
Shaun Robinson <shaunrobinson@Shauns-MacBook-Air-2.local>
2022-02-25 12:10:15 -0800

    Update stories to display appointments and add <Header /> to index.js

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,17 +1,18 @@
 import React from "react";
 
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+
 import "components/Appointment/styles.scss";
 
 export default function Appointment (props) {
 
-  if (!props.time) {
-    return (
-      <article className="appointment" >No Appointment</article>
-    )}
-      else {
-
   return (
-    <article className="appointment" >Appointment at {props.time}</article>
+    
+    <article className="appointment">
+    <Header time={ props.time} />
+    {props.interview ? <Show/> : <Empty/>}
+    </article>
   )
-}
 }
--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from 'react'
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -177,3 +177,10 @@ storiesOf("Button", module)
 
         .add("Create", () =>
           <Form interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")} />)
+
+          .add("Appointment Empty", () => (
+            <Fragment>
+              <Appointment id={1} time="4pm" />
+              <Appointment time="5pm" />
+            </Fragment>
+          ))
