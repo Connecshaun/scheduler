743d4602e48f7e812c9e37d69201f0da16f6572b
Shaun Robinson <shaunrobinson@Shauns-MacBook-Air-2.local>
2022-02-25 13:40:56 -0800

    Add appointment data, import appointment components and insert into <section>

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,28 +1,66 @@
 import React, { useState } from "react";
-
+import Appointment from "components/Appointment";
 import DayList from "./DayList";
-
 import "components/Application.scss";
 
-export default function Application(props) {
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
 
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
+
+export default function Application(props) {
 
   const [day, setDay] = useState("Monday")
 
@@ -35,7 +73,8 @@ export default function Application(props) {
       <img className="sidebar__lhl sidebar--centered" src="images/lhl.png" alt="Lighthouse Labs" />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        { Object.values(appointments).map(appointment => <Appointment key={ appointment.id} {...appointment} />) }
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
