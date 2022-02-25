7388e92538b37b61adaa9bed2513f06a0eafba64
Shaun Robinson <shaunrobinson@Shauns-MacBook-Air-2.local>
2022-02-25 12:30:55 -0800

    Update index to include <Show /> props, along with adding story to test rendering of appointment details

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -8,11 +8,13 @@ import "components/Appointment/styles.scss";
 
 export default function Appointment (props) {
 
+  console.log("props:", props)
+
   return (
     
     <article className="appointment">
     <Header time={ props.time} />
-    {props.interview ? <Show/> : <Empty/>}
+    {props.interview ? <Show student={ props.interview.student } interviewer= { props.interview.interviewer }/> : <Empty/>}
     </article>
   )
 }
