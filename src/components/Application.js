ffd1ce15142a408d4db17490560978789fd8b780
Shaun Robinson <shaunrobinson@Shauns-MacBook-Air-2.local>
2022-03-01 14:17:40 -0800

    Revise 'states' and add setDay and setDays function'

--- src/components/Application.js
+++ src/components/Application.js
@@ -44,8 +44,16 @@ const appointments = {
 };
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
+
+  const setDays = days => setState({ ...state.days})
 
   useEffect(() => {
     Axios.get('/api/days')
@@ -59,7 +67,7 @@ export default function Application(props) {
       <section className="sidebar">
       <img className="sidebar--centered" src="images/logo.png" alt="Interview Scheduler" />
       <hr className="sidebar__separator sidebar--centered" />
-      <nav className="sidebar__menu"><DayList days={ days } value={ day } onChange={ setDay }/></nav>
+      <nav className="sidebar__menu"><DayList days={ state.days } day={ state.day } setDay={ setDay }/></nav>
       <img className="sidebar__lhl sidebar--centered" src="images/lhl.png" alt="Lighthouse Labs" />
       </section>
       <section className="schedule">
