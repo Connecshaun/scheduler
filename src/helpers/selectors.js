11a9e1287b753cdd098bfdb8e0192efd8344bfa3
Shaun Robinson <shaunrobinson@Shauns-MacBook-Air-2.local>
2022-03-01 16:12:29 -0800

    Create getInterview selector and add implement subsequent testing

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,7 +1,5 @@
 
 // Create a function called getAppointmentsForDay that will receive two arguments state and day. The function will return an array of appointments for the given day.
-import React from "react";
-
 export function getAppointmentsForDay (state, day) {
 
   const filteredByDay = state.days.find(givenDay => givenDay.name === day);
@@ -9,4 +7,18 @@ export function getAppointmentsForDay (state, day) {
     return [];
   }
   return filteredByDay.appointments.map(aptID => state.appointments[aptID]);
+}
+
+
+//return an object that contains the interview data if it is passed an object that contains an interviewer.
+export function getInterview(state, interview) {
+  if (!interview) {
+    return null;
+  }
+  return (
+    {
+      student: interview.student,
+      interviewer: state.interviewers[interview.interviewer]
+    }
+  )
 }
