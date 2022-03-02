4bdb7ec19f18ace9346b1f47324d08e400fe5baa
Shaun Robinson <shaunrobinson@Shauns-MacBook-Air-2.local>
2022-03-02 15:02:30 -0800

    Add history array and create a back function and ensure back action via testing

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,9 +2,20 @@ import React, {useState} from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
   const transition = (newMode) => {
     setMode(newMode)
+    history.push(newMode)
   }
+  const back = () => {
+    if (history.length <= 1) {
+      return;
+    }
+    else {
+      history.pop()
+      setMode(history[history.length-1])
+    }
 
-  return { mode, transition }
+  }
+  return { mode, transition, back }
 }
