699fba1cd5c5e65a4af4827f6ed0618d7b442791
Shaun Robinson <shaunrobinson@Shauns-MacBook-Air-2.local>
2022-03-08 15:09:12 -0800

    Update for SAVING

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Status from "components/Appointment/Status";
 import useVisualMode from "../../hooks/useVisualMode";
 import Form from "components/Appointment/Form";
 
@@ -13,12 +14,16 @@ export default function Appointment (props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
-
-  function save(name, interviewer) {
+  const SAVING = "SAVING";
+  
+  const save = function (name, interviewer) {
     const interview = {
       student: name,
       interviewer
     };
+    transition(SAVING);
+    props.bookInterview(props.id, interview)
+    .then(res => transition(SHOW));
   }
 
   const { mode, transition, back } = useVisualMode(props.interview ? SHOW : EMPTY);
@@ -28,7 +33,8 @@ export default function Appointment (props) {
     <Header time={ props.time} />
     { mode === EMPTY && <Empty onAdd={() => transition(CREATE)} /> }
     { mode === SHOW && (<Show student={ props.interview.student } interviewer={ props.interview.interviewer } />)}
-    { mode === CREATE && (<Form interviewers={[props.interviewers]} onCancel={() => back()} />)}
+    { mode === CREATE && (<Form interviewers={[props.interviewers]} onCancel={() => back()} onSave={ save }/>)}
+    { mode === SAVING && (<Status message={ "SAVING" } />)}
     </article>
   )
 }
