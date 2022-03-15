d52fb615fded71565160d6cb3ac580ebf88459d4
Shaun Robinson <shaunrobinson@Shauns-MacBook-Air-2.local>
2022-03-15 13:28:07 -0700

    Modify interviewers function to fix bug in storybook which fixed interviewerlist component along with appointment component

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -4,7 +4,10 @@ import "components/InterviewerList.scss";
 
 export default function InterviewerList (props) {
 
-  const interviewers = props.interviewers[0].map((interviewer) => <InterviewerListItem key = { interviewer.id } name= { interviewer.name } avatar= { interviewer.avatar } selected = { interviewer.id === props.value } setInterviewer = {() => props.onChange(interviewer.id)} />)
+  console.log("INTERVIEWLISTprops:", props)
+
+  const interviewers = props.interviewers.map((interviewer) => {
+  return (<InterviewerListItem key={ interviewer.id }  id={interviewer.id} name= { interviewer.name } avatar= { interviewer.avatar } selected = { interviewer.id === props.value } setInterviewer = {() => props.onChange(interviewer.id)}  />)})
 
   return (
     <section className="interviewers">
